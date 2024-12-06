5cc15b5225923155c4f25ebbba0e7fc73c3cca13
oemer57 <oemer.akin@student.htldornbirn.at>
2024-12-06 16:16:14 +0100

    JS_02

---
+++ 02_Strings_Zahlen/script.js
@@ -0,0 +1,21 @@
+let a = 7.123
+let b = 2.854
+
+let result = a + b
+console.log("a + b = " +result)
+
+let name = "Ömer";
+console.log("Name: " + name)
+
+console.log("Das Ergebnis von " + a + " plus " + b + " ist: " + result)
+
+//Aufgabe aus dem Video:
+let c = 17.4
+let d = 3.9
+result = c / d
+console.log("The result of " + c + " divided by " + d + " ist: " + result)
+
+
+let vorname = "Ömer"
+let nachname = "Akin"
+console.log("Ich heiße " + vorname +" "+ nachname )
