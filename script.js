2b9e2b7fab0fba07ab27aa35bc2fd40cf394528d
oemer57 <oemer.akin@student.htldornbirn.at>
2024-12-06 16:08:21 +0100

    JS_01

---
+++ script.js
@@ -0,0 +1,48 @@
+console.log("Hallo");
+console.log("Hallo nochmal");
+
+let a = 7;
+let b = 8;
+console.log("A hat den Wert " + a);
+
+a = 10;
+console.log("A hat den Wert " + a);
+
+let c = a + b;
+console.log(a+"+"+b +"="+c);
+
+c = a-b;
+console.log(a+"-"+b +"="+c);
+
+
+//Aufgabe aus dem Video:
+// Variablen
+a = 7;
+b = 8;
+c = 10;
+
+
+// Berechne folgendes und gib das Eregebnis über console.log aus
+result = a + b + c;
+console.log("Das Ergebnis von "+a+" + "+b+" + "+c+" ist: " + result);
+
+result = (a + b) / c;
+console.log("Das Ergebnis von ( "+a+" + "+b+" ) / "+c+" ist: " + result);
+
+result = c -a + b;
+console.log("Das Ergebnis von "+c+" - "+a+" + "+b+" ist: " + result);
+
+result = a + b + c * 3;
+console.log("Das Ergebnis von "+a+" + "+b+" + "+c+" * 3 ist: " + result);
+
+result = (a + b + c)/4;
+console.log("Das Ergebnis von ( "+a+" + "+b+" + "+c+" ) / 4 ist: " + result);
+
+
+// Ändere die Variablen auf folgende Werte
+console.log("Variablen vor der Änderung: a = "+a+", b = "+b+", c = "+c);
+a = 3
+b = a + 3
+c = c -3
+// gib die neuen Werte der Variablen aus
+console.log("Variablen nach der Änderung: a = "+a+", b = "+b+", c = "+c);
