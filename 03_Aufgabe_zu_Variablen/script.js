c00ff7ce3fa24abb9c8b1a76b0c8c07f4784965d
oemer57 <oemer.akin@student.htldornbirn.at>
2024-12-13 16:23:00 +0100

    JS_03

---
+++ 03_Aufgabe_zu_Variablen/script.js
@@ -0,0 +1,49 @@
+// Erstelle eine Variable banana mit dem Wert "Banane"
+
+let banana = "Banane";
+
+// Erstelle eine Variable apple mit dem Wert "Apple"
+
+let apple = "Apple";
+
+// Erstelle eine Variable bananaPricePerKilo mit dem Wert 2.14
+
+let bananaPricePerKilo = 2.14;
+
+// Erstelle eine Variable applePricePerKilo mit dem Wert 3.43
+
+let applePricePerKilo = 3.43;
+
+// Ein Apfel hat durchschnittlich ein Gewicht von 0.34 Kilo
+
+let Appleavgweight = 0.34;
+
+// Eine Banane hat durchschnittlich ein Gewicht von 0.22 Kilo
+
+let Bananeavgweight = 0.22;
+
+// Gib folgendes aus: Anzahl Bananen/Äpfel pro Kilo, Preis pro Banane/Apfel
+
+console.log("Anzahl Äpfel pro Kilo "+  1/Appleavgweight)
+
+console.log("Anzahl Bananen pro Kilo "+  1/Bananeavgweight)
+
+console.log(Appleavgweight/1*applePricePerKilo)
+
+console.log(Bananeavgweight/1*bananaPricePerKilo)
+
+// Preis von 8 Äpfeln
+
+console.log((Appleavgweight/1*applePricePerKilo)*8)
+
+// Preis von 17 Bananen
+
+console.log((Bananeavgweight/1*bananaPricePerKilo)*17)
+
+// Preis von 1" Tonne Äpfel
+
+console.log(applePricePerKilo*1000)
+
+// Preis von 1 Tonne Bananen
+
+console.log(bananaPricePerKilo*1000)
